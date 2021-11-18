0092f72e658efdadfcfa093f0cc62ee12b65685c
Alejandro Sáenz <asaenzestrada@gmail.com>
2021-11-17 16:28:42 -0800

    + booking

---
+++ _booking/booking.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Heading</h1>
+<p>My first paragraph.</p>
+<ins class="bookingaff" data-aid="2204933" data-target_aid="2204933" data-prod="map" data-width="100%" data-height="590" data-lang="es" data-currency="MXN" data-dest_id="0" data-dest_type="landmark" data-latitude="32.5320936" data-longitude="-117.0352732" data-landmark_name="Zona Centro" data-mwhsb="0" data-checkin="2021-11-19" data-checkout="2021-11-20" data-address="Zona Centro, Tijuana, Baja California, Mexico">
+    <!-- Anything inside will go away once widget is loaded. -->
+        <a href="//www.booking.com?aid=2204933">Booking.com</a>
+</ins>
+<script type="text/javascript">
+    (function(d, sc, u) {
+      var s = d.createElement(sc), p = d.getElementsByTagName(sc)[0];
+      s.type = 'text/javascript';
+      s.async = true;
+      s.src = u + '?v=' + (+new Date());
+      p.parentNode.insertBefore(s,p);
+      })(document, 'script', 'booking.js');
+</script>
+
+</body>
+
